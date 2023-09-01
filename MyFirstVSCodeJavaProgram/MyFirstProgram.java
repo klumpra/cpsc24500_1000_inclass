8e1588dbdbdb3f6b1b0a2b6ea61923774f530002
klumpra <klumpra@lewisu.edu>
2023-09-01 10:47:23 -0500

    Added another line of output

--- MyFirstVSCodeJavaProgram/MyFirstProgram.java
+++ MyFirstVSCodeJavaProgram/MyFirstProgram.java
@@ -3,5 +3,6 @@ package MyFirstVSCodeJavaProgram;
 public class MyFirstProgram {
     public static void main(String[] args) {
         System.out.println("It is Friday before a long weekend!");
+        System.out.println("It will be hot this weekend.");
     }
 }
